1f1803f1bee853acf7d0a93512fd41cc0058c33d
Roushan Kumar <erroushankrofficial@gmail.com>
2024-06-21 14:04:53 +0530

    Variables done

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,19 @@
+// About different types of variables let, const, var
+
+const num = 34;
+let let_var= "Roushan Kumar";
+var var_variable = "Aman Kumar";
+
+let num1; //by default "undefined" 
+
+num2 = 5; //possible to declare variable like this, but this is not a good practice
+
+/* prefer not to use "var" because of issue in block scope and functional scope  */
+
+console.log(num);
+console.log(num1);
+console.log(num2);
+console.log(let_var);
+console.log(var_variable);
+
+console.table([num, num1, num2, let_var, var_variable]);
