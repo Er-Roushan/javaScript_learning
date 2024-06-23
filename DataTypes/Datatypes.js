4d7a6540f0f3ce99471f51cec1e0de23cc5f1504
Roushan Kumar <erroushankrofficial@gmail.com>
2024-06-23 15:57:06 +0530

    add some more inst in Datatypes.js file

--- DataTypes/Datatypes.js
+++ DataTypes/Datatypes.js
@@ -33,3 +33,52 @@ let x2 = 2/0;
 let y2 = -2/0; 
 console.log(x2);  //infinity
 console.log(y2);  //-infinity
+
+
+//Non primitive type or reference type
+
+//Array
+const arr = ["Roushan Kumar", 2072051, "BFCET"];
+console.log(arr[0]);
+console.log(arr[1]);
+
+console.log(typeof arr); //object type
+
+//Object type
+let obj = {
+    name: "Roushan Kumar", //key value pair
+    uid: 2072051,
+    college: "BFCET"
+}
+
+console.log(obj.name);
+console.log(obj.uid);
+console.log(obj.college);
+
+console.log(typeof obj); //object type
+
+//Function Type
+let func = function() {
+    console.log("Hello World.");
+}
+func();
+
+console.log(typeof func);  // function   ( function object)
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+*/
